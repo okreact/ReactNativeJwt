be3d59ab190bab180d8fcf4fccce8d1344108712
박영일 <baramboy@gmail.com>
2018-02-05 22:10:13 +0900

    Android 테스트 용 코드 작성

--- App.js
+++ App.js
@@ -1,9 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   Platform,
@@ -11,47 +5,111 @@ import {
   Text,
   View
 } from 'react-native';
+import {
+  FormLabel,
+  FormInput,
+  Button,
+  List,
+  ListItem
+} from 'react-native-elements'
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export default class ReactNativeJwt extends Component<{}> {
 
-export default class App extends Component<{}> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+      jwt: '',
+      users: [],
+    }
+  }
+
+  login(e) {
+    //alert(this.state.password);
+    var component = this;
+    fetch("http://localhost:8080/login", {
+      method: "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username: this.state.username,
+        password: this.state.password,
+      })
+    })
+    .then((response) => {
+      if (response.headers.map["authorization"] == undefined)
+        console.log('not!!');
+      else {
+        var jwtString = response.headers.map["authorization"];
+        component.setState({jwt: jwtString})
+
+        fetch("http://localhost:8080/users", {
+          method: "GET",
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + jwtString,
+          }
+        })
+        .then((response) => response.json())
+        .then((json) => {
+          component.setState({users: json});
+        });
+      }
+    })
+  }
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
+    if (this.state.jwt != '') {
+      return (
+        <View style={styles.container}>
+          <Text style={{textAlign: "center"}}>사용자 목록</Text>
+          <List containerStyle={{marginBottom: 20}}>
+            {
+              this.state.users.map((user, i) => (
+                <ListItem
+                  key={i}
+                  title={user.firstName + ' ' + user.lastName}
+                />
+              ))
+            }
+          </List>
+        </View>
+      );
+    }
+    else {
+      return (
+        <View style={styles.container}>
+          <FormLabel>아이디</FormLabel>
+          <FormInput
+            onChangeText={text => this.setState({ username: text })}
+            autoCapitalize="none"
+            value={this.state.username} />
+          <FormLabel>비밀번호</FormLabel>
+          <FormInput
+            onChangeText={text => this.setState({ password: text })}
+            autoCapitalize="none"
+            value={this.state.password} />
+          <View style={{marginTop: 100}}>
+            <Button
+              raised
+              title='로그인'
+              onPress={e => this.login(e)}/>
+            <Text>{this.state.jwt}</Text>
+          </View>
+        </View>
+      );
+    }
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    backgroundColor: '#FFFFFF',
+    paddingTop: 100
   },
 });
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # ReactNativeJwt
+
+React Native 에서 JWT (JSON Web Tokens) 를 이용한 인증 처리 방법을 설명하는 예제 애플리케이션입니다. 애플리케이션을 실행해 보려면 먼저 **React Native** 를 설치하셔야 합니다. 그리고 Android 나 iOS 둘 중 하나는 실행 가능해야 합니다.  
+
+이 애플리케이션에서는 Screen 관리나 토큰 관리 방법 등 실제 애플리케이션에서 필요한 여러 구성 없이 간단하게 구성했습니다. JWT 가 처리되는 방식을 이해하는데 필요한 코드만 작성하였습니다.
+
+실행해 보려면 아래와 같이 준비해 보세요.
+
+1. 서버 측 애플리케이션이 필요합니다. [okreact/jwt-sample-server](https://github.com/okreact/jwt-sample-server) 에 방문하셔서 안내에 따라 서버를 실행합니다.
+3. ``react-native run-ios`` 혹은 ``react-native run-android`` 를 실행합니다. 테스트는 iOS 에서만 했습니다 :)
+
+애플리케이션이 실행되면 아이디와 비밀번호를 입력합니다. ``admin``, ``tiger`` 를 입력합니다. 로그인이 성공하면 앱은 사용자 목록을 리스트 뷰로 출력합니다. 리스트 뷰는 인증이 필요하므로 직접 호출하면 ``403 Forbidden`` 오류가 발생합니다.
